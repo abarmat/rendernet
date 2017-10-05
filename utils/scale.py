0001d3700d033982de4395755d8c228ea482c71b
Ariel Barmat <abarmat@gmail.com>
2017-10-05 12:48:34 -0300

    tools: add script to resize images on a folder

---
+++ utils/scale.py
@@ -0,0 +1,76 @@
+#!/usr/bin/env python
+
+"""Scale and crop multiple images"""
+
+import os.path
+
+from PIL import Image
+
+def resize_and_crop(img_path, modified_path, size, crop_type='top'):
+    """
+    Resize and crop an image to fit the specified size.
+    args:
+        img_path: path for the image to resize.
+        modified_path: path to store the modified image.
+        size: `(width, height)` tuple.
+        crop_type: can be 'top', 'middle' or 'bottom', depending on this
+            value, the image will cropped getting the 'top/left', 'midle' or
+            'bottom/rigth' of the image to fit the size.
+    raises:
+        Exception: if can not open the file in img_path of there is problems
+            to save the image.
+        ValueError: if an invalid `crop_type` is provided.
+    """
+    # If height is higher we resize vertically, if not we resize horizontally
+    img = Image.open(img_path)
+    # Get current and desired ratio for the images
+    img_ratio = img.size[0] / float(img.size[1])
+    ratio = size[0] / float(size[1])
+    #The image is scaled/cropped vertically or horizontally depending on the ratio
+    if ratio > img_ratio:
+        img = img.resize((size[0], size[0] * img.size[1] / img.size[0]),
+                Image.ANTIALIAS)
+        # Crop in the top, middle or bottom
+        if crop_type == 'top':
+            box = (0, 0, img.size[0], size[1])
+        elif crop_type == 'middle':
+            box = (0, (img.size[1] - size[1]) / 2, img.size[0], (img.size[1] + size[1]) / 2)
+        elif crop_type == 'bottom':
+            box = (0, img.size[1] - size[1], img.size[0], img.size[1])
+        else:
+            raise ValueError('ERROR: invalid value for crop_type')
+        img = img.crop(box)
+    elif ratio < img_ratio:
+        img = img.resize((size[1] * img.size[0] / img.size[1], size[1]),
+                Image.ANTIALIAS)
+        # Crop in the top, middle or bottom
+        if crop_type == 'top':
+            box = (0, 0, size[0], img.size[1])
+        elif crop_type == 'middle':
+            box = ((img.size[0] - size[0]) / 2, 0, (img.size[0] + size[0]) / 2, img.size[1])
+        elif crop_type == 'bottom':
+            box = (img.size[0] - size[0], 0, img.size[0], img.size[1])
+        else:
+            raise ValueError('ERROR: invalid value for crop_type')
+        img = img.crop(box)
+    else:
+        img = img.resize((size[0], size[1]),
+                Image.ANTIALIAS)
+        # If the scale is the same, we do not need to crop
+    img.convert('RGB').save(modified_path)
+
+def main():
+    base_path = sys.argv[1]
+    scaled_size = (200, 200)
+
+    for filename in os.listdir(base_path):
+        try:
+            src_path = os.path.join(base_path, filename)
+            dst_path = os.path.join(base_path, 'scaled', filename + '.jpg')
+            resize_and_crop(src_path, dst_path, scaled_size, 'top')
+            print '[M] - %s' % src_path
+        except IOError:
+            print '[E] - %s' % src_path
+
+if __name__ == '__main__':
+    main()
